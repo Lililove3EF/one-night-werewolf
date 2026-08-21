b9ac134a1d809d1bf5a079da68ed9481baff9ed3
lililove <a01031255672@gmail.com>
2026-08-21 13:00:07 +0900

    firebase.js fix

--- firebase.js
+++ firebase.js
@@ -3,63 +3,63 @@ from "https://www.gstatic.com/firebasejs/12.17.0/firebase-app.js";
 
 import {
   getAuth,
-  signInAnonymously,
-  onAuthStateChanged
+  signInAnonymously
 }
 from "https://www.gstatic.com/firebasejs/12.17.0/firebase-auth.js";
 
 import {
-  getDatabase,
-  ref,
-  set,
-  get,
-  update,
-  onValue,
-  push,
-  serverTimestamp
+  getDatabase
 }
 from "https://www.gstatic.com/firebasejs/12.17.0/firebase-database.js";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCLBokqwzJyW2tI2SQqnGHJzxbvcW9648k",
-  authDomain: "onenitewolf.firebaseapp.com",
-  projectId: "onenitewolf",
-  storageBucket: "onenitewolf.firebasestorage.app",
-  messagingSenderId: "242458898688",
-  appId: "1:242458898688:web:626c8dae7dd647255e83e5",
-  measurementId: "G-1V027VHK9W"
+  apiKey: "네 API KEY",
+
+  authDomain:
+    "onenitewolf.firebaseapp.com",
+
+  // ★ Firebase Console의 Realtime Database 주소
+  databaseURL:
+    "여기에 네 Realtime Database URL",
+
+  projectId:
+    "onenitewolf",
+
+  storageBucket:
+    "onenitewolf.firebasestorage.app",
+
+  messagingSenderId:
+    "242458898688",
+
+  appId:
+    "1:242458898688:web:626c8dae7dd647255e83e5",
+
+  measurementId:
+    "G-1V027VHK9W"
 };
 
 
-const app = initializeApp(firebaseConfig);
+const app =
+  initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
 
-export const db = getDatabase(app);
+export const auth =
+  getAuth(app);
+
+
+export const db =
+  getDatabase(app);
 
 
 export async function ensureLogin() {
 
-  if (auth.currentUser)
+  if (auth.currentUser) {
     return auth.currentUser;
+  }
 
-  await signInAnonymously(auth);
+  const credential =
+    await signInAnonymously(auth);
 
-  return new Promise(resolve => {
-
-    const unsub =
-      onAuthStateChanged(auth, user => {
-
-        if (user) {
-
-          unsub();
-
-          resolve(user);
-        }
-
-      });
-
-  });
-
-} 
+  return credential.user;
+}
