66a7964a0680c0f81d9c68ae1a172d9b34e6d8b6
lililove <a01031255672@gmail.com>
2026-08-21 13:10:55 +0900

    fixed all including firebase.js

--- firebase.js
+++ firebase.js
@@ -14,29 +14,14 @@ from "https://www.gstatic.com/firebasejs/12.17.0/firebase-database.js";
 
 
 const firebaseConfig = {
-  apiKey: "네 API KEY",
-
-  authDomain:
-    "onenitewolf.firebaseapp.com",
-
-  // ★ Firebase Console의 Realtime Database 주소
-  databaseURL:
-    "여기에 네 Realtime Database URL",
-
-  projectId:
-    "onenitewolf",
-
-  storageBucket:
-    "onenitewolf.firebasestorage.app",
-
-  messagingSenderId:
-    "242458898688",
-
-  appId:
-    "1:242458898688:web:626c8dae7dd647255e83e5",
-
-  measurementId:
-    "G-1V027VHK9W"
+  apiKey: "AIzaSyCLBokqwzJyW2tI2SQqnGHJzxbvcW9648k",
+  authDomain: "onenitewolf.firebaseapp.com",
+  databaseURL: "https://onenitewolf-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "onenitewolf",
+  storageBucket: "onenitewolf.firebasestorage.app",
+  messagingSenderId: "242458898688",
+  appId: "1:242458898688:web:626c8dae7dd647255e83e5",
+  measurementId: "G-1V027VHK9W"
 };
 
 
